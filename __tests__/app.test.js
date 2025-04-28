368ee0a6544842c5295b074ea3ed0e2c49828598
M <malekuk9@gmail.com>
2025-04-28 12:34:53 +0100

    test for get topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,12 @@
 const endpointsJson = require("../endpoints.json");
 const request = require("supertest");
 const app = require("../controllers/app.controllers");
+const db = require("../db/connection");
 
 // beforeAll(() => {});
-// afterAll(() => {});
+afterAll(() => {
+  db.end();
+});
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -13,5 +16,30 @@ describe("GET /api", () => {
       .then(({ body: { endpoints } }) => {
         expect(endpoints).toEqual(endpointsJson);
       });
+  });
+});
+
+describe("GET /api/topics", () => {
+  it("200: Responds with an array of type topics", async () => {
+    const {
+      body: { topics },
+    } = await request(app).get("/api/topics").expect(200);
+    expect(topics).toHaveLength(3);
+    topics.forEach((topic) => {
+      expect(topic).toMatchObject({
+        slug: expect.any(String),
+        description: expect.any(String),
+      });
+    });
+  });
+  it("Responds with the correct test data", async () => {
+    const {
+      body: { topics },
+    } = await request(app).get("/api/topics");
+    const expected = require("../db/data/test-data/topics");
+    expected.forEach((topic) => {
+      delete topic.img_url;
+    });
+    expect(topics).toEqual(expected);
   });
 });

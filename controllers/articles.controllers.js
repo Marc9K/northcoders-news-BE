7fde0152e8360b1f963cee62466bef04d17e4b8f
M <malekuk9@gmail.com>
2025-04-28 14:28:32 +0100

    feat get article

---
+++ controllers/articles.controllers.js
@@ -0,0 +1,17 @@
+const { selectArticle } = require("../models/articles.models");
+
+exports.getArticle = async (request, response, next) => {
+  const { article_id } = request.params;
+
+  if (isNaN(article_id) || article_id < 1) {
+    return next({ status: 422, msg: "Invalid article_id" });
+  }
+
+  const article = await selectArticle(article_id);
+
+  if (!article) {
+    return next({ status: 404, msg: "Article not found" });
+  }
+
+  return response.status(200).send({ article });
+};

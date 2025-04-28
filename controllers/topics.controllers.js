2666017a8e839841c655acea4c14b257c750d9e6
M <malekuk9@gmail.com>
2025-04-28 12:37:23 +0100

    refactor error handle

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,10 +1,6 @@
 const { selectAllTopics } = require("../models/topics.model");
 
 exports.getTopics = async (_, response) => {
-  try {
-    const topics = await selectAllTopics();
-    response.status(200).send({ topics });
-  } catch (error) {
-    console.log(error);
-  }
+  const topics = await selectAllTopics();
+  response.status(200).send({ topics });
 };

d994fc8a3fc624427b536181030672d0eaaa0e24
M <malekuk9@gmail.com>
2025-04-29 13:37:37 +0100

    feat voting for article

--- models/articles.models.js
+++ models/articles.models.js
@@ -19,3 +19,11 @@ exports.selectAllArticles = async () => {
   );
   return articlesQuery.rows;
 };
+
+exports.updateArticle = async (article_id, inc_votes) => {
+  const articleQuery = await db.query(
+    `UPDATE articles SET votes = votes + $1 WHERE article_id = $2 RETURNING *`,
+    [inc_votes, article_id]
+  );
+  return articleQuery.rows[0];
+};

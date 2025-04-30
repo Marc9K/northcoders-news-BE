ac85ab746c4d5d39f931744c757ab756af4045ee
M <malekuk9@gmail.com>
2025-04-30 13:36:56 +0100

    feat topic query for articles

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,4 +1,6 @@
 const db = require("../db/connection.js");
+const { selectTopic } = require("./topics.models.js");
+
 exports.selectArticle = async (article_id) => {
   const articlesWithIdQuery = await db.query(
     `SELECT * FROM articles WHERE article_id = $1`,
@@ -7,7 +9,7 @@ exports.selectArticle = async (article_id) => {
   return articlesWithIdQuery.rows[0];
 };
 
-exports.selectAllArticles = async (sortByColumn, order) => {
+exports.selectAllArticles = async (sortByColumn, order, topic) => {
   let sqlOrder = "DESC";
   if (order && order.toLowerCase() === "asc") {
     sqlOrder = "ASC";
@@ -25,15 +27,25 @@ exports.selectAllArticles = async (sortByColumn, order) => {
   ) {
     sqlSortByColumn = sortByColumn;
   }
-  const articlesQuery = await db.query(
-    `SELECT 
+  let sqlArgs = [];
+  let sqlQuery = `SELECT 
       articles.author, title, articles.article_id, topic, articles.created_at, articles.votes, article_img_url, 
       CAST(COUNT(comments.comment_id) AS INTEGER) AS comment_count 
       FROM articles 
       LEFT JOIN comments ON articles.article_id = comments.article_id 
-      GROUP BY articles.article_id 
-      ORDER BY ${sqlSortByColumn} ${sqlOrder}`
-  );
+      `;
+  if (topic) {
+    if (!(await selectTopic(topic))) {
+      return Promise.reject({ status: 404, msg: "Topic not found" });
+    }
+    sqlQuery += `WHERE topic = $1
+        `;
+    sqlArgs.push(topic);
+  }
+  sqlQuery += `GROUP BY articles.article_id 
+      ORDER BY ${sqlSortByColumn} ${sqlOrder};`;
+
+  const articlesQuery = await db.query(sqlQuery, sqlArgs);
   return articlesQuery.rows;
 };
 

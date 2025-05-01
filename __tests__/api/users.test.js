63dcb9b2b62819d285e94ea806a104132124622d
M <malekuk9@gmail.com>
2025-05-01 11:51:41 +0100

    test and description for getting user

--- __tests__/api/users.test.js
+++ __tests__/api/users.test.js
@@ -29,3 +29,33 @@ describe("/api/users/", () => {
     });
   });
 });
+
+describe("/api/users/:username", () => {
+  test("200: Responds with an existing user", async () => {
+    const {
+      body: { user },
+    } = await request(app).get("/api/users/butter_bridge").expect(200);
+    expect(user).toMatchObject({
+      username: expect.any(String),
+      name: expect.any(String),
+      avatar_url: expect.any(String),
+    });
+  });
+  test("Responds with correct user data", async () => {
+    const {
+      body: { user },
+    } = await request(app).get("/api/users/butter_bridge");
+    expect(user).toEqual({
+      username: "butter_bridge",
+      name: "jonny",
+      avatar_url:
+        "https://www.healthytherapies.com/wp-content/uploads/2016/06/Lime3.jpg",
+    });
+  });
+  test("404: Responds with an error if user does not exist", async () => {
+    const {
+      body: { msg },
+    } = await request(app).get("/api/users/not_a_user").expect(404);
+    expect(msg).toBe("User not found");
+  });
+});

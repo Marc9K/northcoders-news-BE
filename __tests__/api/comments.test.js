7870a26304b95566c2688d28c9e6cbef6d6eb684
M <malekuk9@gmail.com>
2025-05-01 13:16:05 +0100

    test and description for voting for a comment

--- __tests__/api/comments.test.js
+++ __tests__/api/comments.test.js
@@ -37,3 +37,54 @@ describe("/api/comments/", () => {
     });
   });
 });
+
+describe("/api/comments/:comment_id", () => {
+  describe("PATCH", () => {
+    it("200: Responds with the updated comment", async () => {
+      const { body } = await request(app)
+        .patch("/api/comments/1")
+        .send({ inc_votes: 1 })
+        .expect(200);
+      expect(body.comment).toEqual({
+        comment_id: 1,
+        body: "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+        votes: 17,
+        author: "butter_bridge",
+        article_id: 9,
+        created_at: expect.any(String),
+      });
+    });
+    it("Updates with negative numbers", async () => {
+      const {
+        body: {
+          comment: { votes },
+        },
+      } = await request(app)
+        .patch("/api/comments/1")
+        .send({ inc_votes: -1 })
+        .expect(200);
+      expect(votes).toBe(15);
+    });
+    it("404: Responds with Not found if no such comment ever existed", async () => {
+      await request(app)
+        .patch("/api/comments/200")
+        .send({ inc_votes: 1 })
+        .expect(404);
+    });
+    it("400: Responds with Invalid input if comment_id is invalid", async () => {
+      await request(app)
+        .patch("/api/comments/aaa")
+        .send({ inc_votes: 1 })
+        .expect(400);
+    });
+    it("400: Responds with Invalid input if inc_votes is invalid", async () => {
+      await request(app)
+        .patch("/api/comments/1")
+        .send({ inc_votes: "aaa" })
+        .expect(400);
+    });
+    it("400: Responds with Invalid input if inc_votes is missing", async () => {
+      await request(app).patch("/api/comments/1").send({}).expect(400);
+    });
+  });
+});

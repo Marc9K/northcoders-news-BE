1a998ff4cc97c7c6d5271a22e2106e74c4b2b5d2
M <malekuk9@gmail.com>
2025-05-02 19:00:00 +0100

    feat posting topic

---
+++ controllers/routers/topics.js
@@ -0,0 +1,7 @@
+const topicsRouter = require("express").Router();
+
+const { getTopics, postTopic } = require("../topics.controllers");
+
+topicsRouter.route("/").get(getTopics).post(postTopic);
+
+module.exports = topicsRouter;
--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,6 +1,16 @@
-const { selectAllTopics } = require("../models/topics.models");
+const { selectAllTopics, insertTopic } = require("../models/topics.models");
 
 exports.getTopics = async (_, response) => {
   const topics = await selectAllTopics();
   response.status(200).send({ topics });
+};
+
+exports.postTopic = async (request, response, next) => {
+  try {
+    const topicToPost = request.body;
+    const topic = await insertTopic(topicToPost);
+    response.status(201).send({ topic });
+  } catch (error) {
+    next(error);
+  }
 };
